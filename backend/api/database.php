ff92d6822f3cc33f2aec1a93ee59c1c1e070b4d1
muthukuttiraja <muthukuttiraja@gmail.com>
2021-09-30 12:17:12 -0600

    sec

--- backend/api/database.php
+++ backend/api/database.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'P@$$w0rd');
+define('DB_PASS', '');
 define('DB_NAME', 'tasktracker');
 
 function connect()
